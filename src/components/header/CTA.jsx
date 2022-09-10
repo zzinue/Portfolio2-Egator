532834267b6fc9cd015ccbbf9dc4ad9c1d685a33
zzinue <zzinue@gmail.com>
2022-09-10 10:32:27 -0500

    adding buttons of contact

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CV from '../../assets/CV.pdf'
+
+
+const CTA = () => {
+    return (
+        <div className='cta'>
+            <a href={CV} className="btn">View CV</a>
+            <a href="#contact" className='btn btn-primary'>Let's talk</a>
+        </div>
+    )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Victor Guerrero</h1>
+                <h5 className="text-light">
+                    FullStack Developer
+                </h5>
+                <CTA />
+
+            </div>
+        </header>
     )
 }
 
