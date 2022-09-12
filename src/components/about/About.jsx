3fa130b87c6b988a9d53471d73128a1d89704924
zzinue <zzinue@gmail.com>
2022-09-11 23:26:21 -0500

    styl grid about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import './About.css'
-
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 const About = () => {
     return (
-        <div>About</div>
+        <section id="about">
+            <h5>Get to Know</h5>
+            <h2>About me</h2>
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={ME} alt="About Image" />
+                    </div>
+                </div>
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className='about__card'>
+                            <FaAward className='about__icon' />
+                            <h5>Experience</h5>
+                            <small>1+ Year Working</small>
+                        </article>
+                        <article className='about__card'>
+                            <FiUsers className='about__icon' />
+                            <h5>Clients</h5>
+                            <small>10+ local</small>
+                        </article>
+                        <article className='about__card'>
+                            <VscFolderLibrary className='about__icon' />
+                            <h5>Projecst</h5>
+                            <small>20+ Completed</small>
+                        </article>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde sapiente itaque reiciendis tenetur mollitia deleniti a, nobis dolorum, fugit qui quidem eaque accusantium cupiditate non voluptatibus consequatur id libero laboriosam?</p>
+                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
+        </section>
     )
 }
 
