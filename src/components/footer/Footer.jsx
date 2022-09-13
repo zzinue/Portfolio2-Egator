d532bbbf50128cdc613f79d4ce96f786cbfb9b8e
zzinue <zzinue@gmail.com>
2022-09-13 10:49:08 -0500

    finishing portfolio

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import './Footer.css'
+import { RiFacebookFill } from 'react-icons/ri'
+import { BsInstagram } from 'react-icons/bs'
+import { SiTwitter } from 'react-icons/si'
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <a href="#" className='footer__logo'>Victor</a>
+            <ul className='permalinks'>
+                <li><a href="#">Home</a> </li>
+                <li><a href="#about">About</a> </li>
+                <li><a href="#experience">Experience</a> </li>
+                <li><a href="#services">Services</a> </li>
+                <li><a href="#portfolio">Portfolio</a> </li>
+                <li><a href="#testimonials">Testimonials</a> </li>
+                <li><a href="#contact">Contact</a> </li>
+            </ul>
+            <div className="footer__socials">
+                <a href="https://facebook.com"><RiFacebookFill /></a>
+                <a href="https://instagram.com"><BsInstagram /></a>
+                <a href="https://twitter.com"><SiTwitter /></a>
+            </div>
+            <div className="footer__copyright">
+                <small>&copy; Victor Guerrero. All rights reserved.</small>
+            </div>
+        </footer>
     )
 }
 
